7234f17de7d21d7ef4749d92cd6dd64764e600d7
Konrad Sniatala <k.sniatala@gmail.com>
2017-04-08 20:16:52 +0200

    Tutorial 2 page

--- io2_project/settings.py
+++ io2_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eleague.apps.EleagueConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
