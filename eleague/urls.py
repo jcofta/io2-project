20c3f97b8b9d927570e66676127de6b9846035f4
Konrad Sniatala <k.sniatala@gmail.com>
2017-04-26 19:12:35 +0200

    Added user tournaments "page"

--- eleague/urls.py
+++ eleague/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # ex: /eleague/
     url(r'^$', views.index, name='index'),
     # # ex: /eleague/5/
-    url(r'^myevents/$',views.myevents, name='myevents')
+    url(r'^myevents/$',views.myevents, name='myevents'),
+    url(r'^mytournaments/$',views.mytournaments, name='mytournaments')
     # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
 ]
--- eleague/views.py
+++ eleague/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404,render
 from django.http import Http404
 from django.http import HttpResponse
-from .models import Event
+from .models import Event, Tournament
 from django.contrib.auth import models as auth_models
 
 
@@ -15,7 +15,11 @@ def myevents(request):
     event_list = Event.objects.filter(tournament__owner=request.user)
     context = {'event_list': event_list}
     return render(request, 'eleague/myevents.html',context)
-#
+
+def mytournaments(request):
+    tournament_list = Tournament.objects.filter(owner=request.user)
+    context = {'tournament_list': tournament_list}
+    return render(request, 'eleague/mytournaments.html',context)
 # def results(request, question_id):
 #     response = "You're looking at the results of question %s."
 #     return HttpResponse(response % question_id)
